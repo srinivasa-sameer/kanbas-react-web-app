f6741a7eb70b7d7d993ef994831761efda675ec5
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2023-10-07 13:25:12 -0400

    Lab 3 Final Update

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -3,11 +3,13 @@ import PathParameters from './PathParameters';
 import Classes from './Classes/index';
 import Styles from './Styles';
 import ConditionalOutput from './ConditionalOutput';
+import TodoList from './todo/TodoList';
 
 const Assignment3 = () => {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <TodoList />
       <ConditionalOutput />
       <Styles />
       <Classes />
